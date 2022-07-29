35f6339ddea92043e9316e8ba660d16b4763f134
MonnalisaFidelis <monnalisafidelis@gmail.com>
2022-07-29 10:21:51 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
-console.log(validator);
+  
+  let botao = document.querySelector("#botao")
+  let respCard = document.querySelector("#altCard")
+
+  botão.addEventListerner("click", validadorCard)
+
+  function validadorCard(){
+    let numCard = Number(document.querySelector("#numeroDumerooCartao").value)
+    console.log(numCard)
+
+    respCard.innerText = numCard
+
+  }
+
+   
+
+    //Função p colocar espaço entre os numeros do cartão
+   /* const input = document.querySelector("#input-text")
+
+    input.addEventListerner("keypress",()=> {
+      let inputlength = input.value.le
+    })
