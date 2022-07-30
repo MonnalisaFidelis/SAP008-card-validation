14314c21f743c860edb1b707eee87d632e5df73d
MonnalisaFidelis <monnalisafidelis@gmail.com>
2022-07-29 23:33:12 -0300

    alteracao np codigo

--- src/index.js
+++ src/index.js
@@ -1,39 +1,24 @@
 import validator from './validator.js';
 
-  
-  let botaoCard = document.querySelector("#botao");
-  // variavel criada para trazer as informações do botão
-  let respCard = document.querySelector("#respostaCartao");
-//variavel criada para exibir a resposta do usuario na tela
-  botaoCard.addEventListerner("click", validatorCard);
-// cria um evento tipo click para chamar a função 
-  
-function validatorCard(){
-    let numCard = document.querySelector("#numCartao").value;
+let btnCard = document.querySelector("#botao");
+let respCard = document.querySelector("#outCard");
+
+btnCard.addEventListener('click', validadorCard);
+
+function validadorCard() {
+    let numCard = document.querySelector("#numDigitado").value; // parseInt()
     console.log(numCard);
-    /*let resulttadoFinal = validadorCard.isValid(numCard);
-    
-  if (numCard ==="") {
-    return respCard.innerText = " ENTRE COM UM NÚMERO";
+    let resultadoFinal = validator.isValid(numCard); // argumento
 
-  }
-  else if (resulttadoFinal == true) {
-    respCard.innerText = numCard + "CARTÃO VALIDO";
-  }
-  else {
-    respCard.innerText = numCard + "CARTÃO INVÁLIDO";
-    console.log(resulttadoFinal)
-  }*/
-
-  }
-
-   
-
-
-
-    //Função p colocar espaço entre os numeros do cartão
-   /* const input = document.querySelector("#input-text")
-
-    input.addEventListerner("keypress",()=> {
-      let inputlength = input.value.le
-    }*/
+    if (numCard === "") {
+        return respCard.innerText = " ENTRE COM UM NÚMERO";
+    }
+    else if (resultadoFinal == true) {
+        respCard.innerText = numCard + " CARTÃO VÁLIDO";
+        console.log(resultadoFinal)
+    }
+    else {
+        respCard.innerText = numCard + " CARTÃO INVÁLIDO";
+        console.log(resultadoFinal)
+    }
+}
